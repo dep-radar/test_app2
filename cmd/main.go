c7530d7fe49962564528858d74f4e33f13dacf24
Rustam Zagirov <stammru@gmail.com>
2018-02-09 16:07:10 +0300

    add package with revision

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 
 	"github.com/pkg/errors"
+	"github.com/pkg/profile"
 	"github.com/pkg/sftp"
 )
 
 func main() {
+	defer profile.Start().Stop()
 	err := stderr.New("my")
 	err = errors.Wrap(err, "my wrap")
 	fmt.Printf("Error: %s %d", err.Error(), sftp.SftpServerWorkerCount)
