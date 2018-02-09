2b06ee862467b26d6d863f182e30ff1a4b61099e
Rustam Zagirov <stammru@gmail.com>
2018-02-09 17:02:22 +0300

    remove sftp

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,11 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/pkg/profile"
-	"github.com/pkg/sftp"
 )
 
 func main() {
 	defer profile.Start().Stop()
 	err := stderr.New("my")
 	err = errors.Wrap(err, "my wrap")
-	fmt.Printf("Error: %s %d", err.Error(), sftp.SftpServerWorkerCount)
+	fmt.Printf("Error: %s", err.Error())
 }
